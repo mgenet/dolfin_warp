503b4ff04e045ebf9aaecb8a2b0d5d02d0ce7106
Martin Genet <martin@mortis.local>
2018-09-07 22:33:59 +0200

    Main function has been split into mutliple classes. fedic2 has the same interface as original fedic function, but uses the new architecture.

---
+++ Problem.py
@@ -0,0 +1,20 @@
+#coding=utf8
+
+################################################################################
+###                                                                          ###
+### Created by Martin Genet, 2016-2018                                       ###
+###                                                                          ###
+### École Polytechnique, Palaiseau, France                                   ###
+###                                                                          ###
+################################################################################
+
+import myPythonLibrary as mypy
+import myVTKPythonLibrary as myvtk
+
+import dolfin_dic as ddic
+
+################################################################################
+
+class Problem():
+
+        pass
