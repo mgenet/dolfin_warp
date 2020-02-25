9901c16c95004daa865dbc1a3c0949369bfa4ffc
Martin Genet <martin.genet@polytechnique.edu>
2020-02-26 00:22:57 +0100

    First implementation of weak equilibrium gap formulation

--- Energy_Regularization.py
+++ Energy_Regularization.py
@@ -35,6 +35,7 @@ class RegularizationEnergy(Energy):
             quadrature_degree=None):
 
         self.problem           = problem
+        self.dim               = self.problem.U.ufl_shape[0]
         self.printer           = problem.printer
         self.name              = name
         self.w                 = w
@@ -103,7 +104,6 @@ class RegularizationEnergy(Energy):
                     parameters=self.material_parameters)
             self.Psi_m, self.S_m = self.material.get_free_energy(
                 U=self.problem.U)
-            self.dim = self.problem.U.ufl_shape[0]
             self.I = dolfin.Identity(self.dim)
             self.F = self.I + dolfin.grad(self.problem.U)
             self.P_m = self.F * self.S_m
@@ -117,36 +117,156 @@ class RegularizationEnergy(Energy):
             self.Psi_m_F = dolfin.Constant(0)
             self.Psi_m_S = dolfin.Constant(0)
         elif (self.type == "equilibrated"):
-            self.Div_P = dolfin.div(self.P_m)
-            self.Psi_m_V = dolfin.inner(self.Div_P,
-                                        self.Div_P)
-            self.N = dolfin.FacetNormal(self.problem.mesh)
-            self.Jump_P_N = dolfin.jump(self.P_m,
-                                        self.N)
-            self.cell_h = dolfin.Constant(self.problem.mesh.hmin())
-            self.Psi_m_F = dolfin.inner(self.Jump_P_N,
-                                        self.Jump_P_N)/self.cell_h
-            # self.P_N = self.P_m * self.N
-            # self.P_N_N = dolfin.dot(self.N,
-            #                         self.P_N)
-            # self.P_N_T = self.P_N - self.P_N_N * self.N
-            # self.Psi_m_S = dolfin.inner(self.P_N_T,
-            #                             self.P_N_T)/self.cell_h
-            # self.Psi_m_S = dolfin.inner(self.P_N,
-            #                             self.P_N)/self.cell_h
+            Div_P = dolfin.div(self.P_m)
+            self.Psi_m_V = dolfin.inner(
+                Div_P,
+                Div_P)/2
+            N = dolfin.FacetNormal(self.problem.mesh)
+            Jump_P_N = dolfin.jump(
+                self.P_m,
+                N)
+            cell_h = dolfin.Constant(self.problem.mesh.hmin())
+            self.Psi_m_F = dolfin.inner(
+                Jump_P_N,
+                Jump_P_N)/2/cell_h
+            # self.P_N = self.P_m * N
+            # self.P_N_N = dolfin.dot(N, self.P_N)
+            # self.P_N_T = self.P_N - self.P_N_N * N
+            # self.Psi_m_S = dolfin.inner(
+            #     self.P_N_T,
+            #     self.P_N_T)/2/cell_h
+            # self.Psi_m_S = dolfin.inner(
+            #     self.P_N,
+            #     self.P_N)/2/cell_h
+            self.Psi_m_S = dolfin.Constant(0)
+        elif (self.type == "equilibrated2"):
+            # print(self.P_m)
+            # print("P_m.ufl_shape = "+str(self.P_m.ufl_shape))
+            self.dP_m  = dolfin.diff(self.P_m , self.problem.U)
+            self.ddP_m = dolfin.diff(self.dP_m, self.problem.U)
+            # print(self.dP_m)
+            # print("dP_m.ufl_shape = "+str(self.dP_m.ufl_shape))
+            # print(self.ddP_m)
+            # print("ddP_m.ufl_shape = "+str(self.ddP_m.ufl_shape))
+
+            self.P_fe = dolfin.TensorElement(
+                family="Lagrange",
+                cell=self.problem.mesh.ufl_cell(),
+                shape=(self.dim,)*2,
+                degree=self.problem.U_degree)
+            self.P_fs = dolfin.FunctionSpace(
+                self.problem.mesh,
+                self.P_fe)
+            self.P_func = dolfin.Function(
+                self.P_fs,
+                name="stress")
+            # print("P_func.ufl_shape = "+str(self.P_func.ufl_shape))
+            self.dP_fe = dolfin.TensorElement(
+                family="Lagrange",
+                cell=self.problem.mesh.ufl_cell(),
+                shape=(self.dim,)*3,
+                degree=self.problem.U_degree)
+            self.dP_fs = dolfin.FunctionSpace(
+                self.problem.mesh,
+                self.dP_fe)
+            self.dP_func = dolfin.Function(
+                self.dP_fs,
+                name="dstress")
+            # print("dP_func.ufl_shape = "+str(self.dP_func.ufl_shape))
+            self.ddP_fe = dolfin.TensorElement(
+                family="Lagrange",
+                cell=self.problem.mesh.ufl_cell(),
+                shape=(self.dim,)*4,
+                degree=self.problem.U_degree)
+            self.ddP_fs = dolfin.FunctionSpace(
+                self.problem.mesh,
+                self.ddP_fe)
+            self.ddP_func = dolfin.Function(
+                self.ddP_fs,
+                name="ddstress")
+            # print("ddP_func.ufl_shape = "+str(self.ddP_func.ufl_shape))
+
+            Div_P = dolfin.div(self.P_func)
+            # print("Div_P.ufl_shape = "+str(self.Div_P.ufl_shape))
+            self.Psi_m_V = dolfin.inner(
+                Div_P,
+                Div_P)/2
+            # print(self.Psi_m_V)
+            self.Psi_m_F = dolfin.Constant(0)
+            self.Psi_m_S = dolfin.Constant(0)
+        elif (self.type == "equilibrated3"):
+            self.Psi_m_V = dolfin.Constant(0)
+            self.Psi_m_F = dolfin.Constant(0)
             self.Psi_m_S = dolfin.Constant(0)
         else:
             assert (0), "\"type\" ("+str(self.type)+") must be \"hyperelastic\" or \"equilibrated\". Aborting."
 
-        self.DPsi_m_V  = dolfin.derivative( self.Psi_m_V, self.problem.U, self.problem.dU_test )
-        self.DPsi_m_F  = dolfin.derivative( self.Psi_m_F, self.problem.U, self.problem.dU_test )
-        self.DPsi_m_S  = dolfin.derivative( self.Psi_m_S, self.problem.U, self.problem.dU_test )
-        self.DDPsi_m_V = dolfin.derivative(self.DPsi_m_V, self.problem.U, self.problem.dU_trial)
-        self.DDPsi_m_F = dolfin.derivative(self.DPsi_m_F, self.problem.U, self.problem.dU_trial)
-        self.DDPsi_m_S = dolfin.derivative(self.DPsi_m_S, self.problem.U, self.problem.dU_trial)
+        if (self.type in ("hyperelastic", "equilibrated")):
+            self.DPsi_m_V  = dolfin.derivative( self.Psi_m_V, self.problem.U, self.problem.dU_test )
+            self.DPsi_m_F  = dolfin.derivative( self.Psi_m_F, self.problem.U, self.problem.dU_test )
+            self.DPsi_m_S  = dolfin.derivative( self.Psi_m_S, self.problem.U, self.problem.dU_test )
+            self.DDPsi_m_V = dolfin.derivative(self.DPsi_m_V, self.problem.U, self.problem.dU_trial)
+            self.DDPsi_m_F = dolfin.derivative(self.DPsi_m_F, self.problem.U, self.problem.dU_trial)
+            self.DDPsi_m_S = dolfin.derivative(self.DPsi_m_S, self.problem.U, self.problem.dU_trial)
+        elif (self.type == "equilibrated2"):
+            # print(self.dP_func.ufl_shape)
+            # print(self.problem.dU_test.ufl_shape)
+            Div_dPtest = dolfin.div(dolfin.dot(self.dP_func, self.problem.dU_test))
+            # print(Div_dPtest)
+            Div_dPtrial = dolfin.div(dolfin.dot(self.dP_func, self.problem.dU_trial))
+            # print(Div_dPtrial)
+            Div_ddPtesttrial = dolfin.div(dolfin.dot(dolfin.dot(self.ddP_func, self.problem.dU_test), self.problem.dU_trial))
+            # print(Div_ddPtesttrial)
+            self.DPsi_m_V = dolfin.inner(
+                Div_P,
+                Div_dPtest)
+            # print(self.DPsi_m_V)
+            self.DPsi_m_F = dolfin.derivative(dolfin.Constant(0), self.problem.U, self.problem.dU_test) # MG20200225: Needs to be linear in dU_test (arity = 1)
+            self.DPsi_m_S = dolfin.derivative(dolfin.Constant(0), self.problem.U, self.problem.dU_test) # MG20200225: Needs to be linear in dU_test (arity = 1)
+            self.DDPsi_m_V = dolfin.inner(
+                Div_dPtrial,
+                Div_dPtest)
+            # print(self.DDPsi_m_V)
+            self.DDPsi_m_V += dolfin.inner(
+                Div_P,
+                Div_ddPtesttrial)
+            # print(self.DDPsi_m_V)
+            self.DDPsi_m_F = dolfin.derivative(self.DPsi_m_F, self.problem.U, self.problem.dU_trial) # MG20200225: Needs to be linear in dU_test and dU_trial (arity = 2)
+            self.DDPsi_m_S = dolfin.derivative(self.DPsi_m_S, self.problem.U, self.problem.dU_trial) # MG20200225: Needs to be linear in dU_test and dU_trial (arity = 2)
+        elif (self.type == "equilibrated3"):
+            self.DPsi_m_V = dolfin.derivative(        self.Psi_m, self.problem.U, self.problem.dU_test)
+            self.DPsi_m_F = dolfin.derivative(dolfin.Constant(0), self.problem.U, self.problem.dU_test) # MG20200225: Needs to be linear in dU_test (arity = 1)
+            N = dolfin.FacetNormal(self.problem.mesh)
+            self.DPsi_m_S = - dolfin.inner(
+                self.P_m * N,
+                self.problem.dU_test)
+            self.DDPsi_m_V = dolfin.derivative(self.DPsi_m_V, self.problem.U, self.problem.dU_trial)
+            self.DDPsi_m_F = dolfin.derivative(self.DPsi_m_F, self.problem.U, self.problem.dU_trial) # MG20200225: Needs to be linear in dU_test and dU_trial (arity = 2)
+            self.DDPsi_m_S = dolfin.derivative(self.DPsi_m_S, self.problem.U, self.problem.dU_trial)
+        else:
+            assert (0), "\"type\" ("+str(self.type)+") must be \"hyperelastic\" or \"equilibrated\". Aborting."
 
         self.ener_form =   self.Psi_m_V * self.dV +   self.Psi_m_F * self.dF +   self.Psi_m_S * self.dS
         self.res_form  =  self.DPsi_m_V * self.dV +  self.DPsi_m_F * self.dF +  self.DPsi_m_S * self.dS
         self.jac_form  = self.DDPsi_m_V * self.dV + self.DDPsi_m_F * self.dF + self.DDPsi_m_S * self.dS
 
         self.printer.dec()
+
+
+
+    def call_before_assembly(self,
+            **kwargs):
+
+        if (self.type == "equilibrated2"):
+            dolfin.project(
+                v=self.P_m,
+                V=self.P_fs,
+                function=self.P_func)
+            dolfin.project(
+                v=self.dP_m,
+                V=self.dP_fs,
+                function=self.dP_func)
+            dolfin.project(
+                v=self.ddP_m,
+                V=self.ddP_fs,
+                function=self.ddP_func)
