65105266bd3d558a8c04b8090866f6438ae5653f
Ezgi Berberoglu <berberoglu@biomed.ee.ethz.ch>
2021-08-10 18:21:53 +0200

    Functions and examples scripts for 2021 PLoS ONE paper

---
+++ Examples/[Berberoglu et al., 2021, PLoS ONE]/test_compute_best_beta.py
@@ -0,0 +1,35 @@
+#coding=utf8
+
+########################################################################
+###                                                                  ###
+### Created by Ezgi Berberoglu, 2017-2021                            ###
+###                                                                  ###
+### Swiss Federal Institute of Technology (ETH), Zurich, Switzerland ###
+### École Polytechnique, Palaiseau, France                           ###
+###                                                                  ###
+########################################################################
+
+import dolfin_warp as dwarp
+
+########################################################################
+
+folders  = []
+# folders += ["resolution_1.0x1.0_noNoise"]
+# folders += ["resolution_1.5x1.5_noNoise"]
+# folders += ["resolution_2.0x2.0_noNoise"]
+# folders += ["resolution_2.5x2.5_noNoise"]
+folders += ["resolution_3.0x3.0_noNoise"]
+# folders += ["resolution_3.5x3.5_noNoise"]
+
+betas = ["0","0.1","0.2","0.3","0.4","0.5","0.025","0.05","0.075"]
+noisy = 0
+es_tf = 55
+
+for k_folder in range(len(folders)):
+    folder = folders[k_folder]
+    print "Displacement error computation for: " + folder
+    dwarp.compute_best_beta(
+        es_tf,
+        betas,
+        folder,
+        noisy)
---
+++ Examples/[Berberoglu et al., 2021, PLoS ONE]/test_compute_strain_error_mean_stdev.py
@@ -0,0 +1,33 @@
+#coding=utf8
+
+########################################################################
+###                                                                  ###
+### Created by Ezgi Berberoglu, 2017-2021                            ###
+###                                                                  ###
+### Swiss Federal Institute of Technology (ETH), Zurich, Switzerland ###
+### École Polytechnique, Palaiseau, France                           ###
+###                                                                  ###
+########################################################################
+
+import dolfin_warp as dwarp
+
+########################################################################
+
+folders  = []
+# folders += ["resolution_1.0x1.0_noNoise"]
+# folders += ["resolution_1.5x1.5_noNoise"]
+# folders += ["resolution_2.0x2.0_noNoise"]
+# folders += ["resolution_2.5x2.5_noNoise"]
+folders += ["resolution_3.0x3.0_noNoise"]
+# folders += ["resolution_3.5x3.5_noNoise"]
+
+k_frame = 55
+noisy = 0
+
+for k_folder in range(len(folders)):
+    folder = folders[k_folder]
+    print "Strain error computation for: " + folder
+    dwarp.compute_strain_error(
+        k_frame,
+        folder,
+        noisy)
