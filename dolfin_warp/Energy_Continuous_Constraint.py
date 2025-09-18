6579ec07d44316bfaf1a9506d7f58462542d5a15
Martin Genet <martin.genet@polytechnique.edu>
2025-09-18 09:59:51 +0200

    Typo (w/ Nagham)

--- dolfin_warp/Energy_Continuous_Constraint.py
+++ dolfin_warp/Energy_Continuous_Constraint.py
@@ -65,7 +65,7 @@ class ConstraintContinuousEnergy(ContinuousEnergy):
             "ds",
             domain=self.problem.mesh,
             subdomain_data=surface_subdomain_data,
-            subdomain_id=surface_subdomain_id if volume_subdomain_id is not None else "everywhere",
+            subdomain_id=surface_subdomain_id if surface_subdomain_id is not None else "everywhere",
             metadata=self.form_compiler_parameters)
 
         self.printer.print_str("Defining constraint energy…")
