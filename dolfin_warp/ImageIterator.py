d93a4f43c7eb1545269f13a5d7f93838aa36cffb
Parsa Namazian <parsa@mustafar.polytechnique.fr>
2026-04-23 12:09:06 +0200

    Add printing vector of disp

--- dolfin_warp/ImageIterator.py
+++ dolfin_warp/ImageIterator.py
@@ -231,7 +231,7 @@ class ImageIterator():
 
                 # Add bool to print or not and check that in reduced model and not full kinematics
                 if self.save_reduced_disp:
-                    assert self.problem.kinematics_type=="reduced", "Not reduced displacement for full kinematics model"
+                    # assert self.problem.kinematics_type=="reduced", "Not reduced displacement for full kinematics model"
                     self.printer.print_str("Wrtiting reduced-disp vector")
                     print(f"self.problem.reduced_displacement.vector()[:] is: {self.problem.reduced_displacement.vector()[:]}") #DEBUG
                     numpy.savetxt(self.working_folder+"/"+self.working_basename+"_reduced_kinematics.dat", numpy.array([self.problem.reduced_displacement.vector()[:]]))                  
